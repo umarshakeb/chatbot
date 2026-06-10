ee82f3786a430d46dfe7ed030256f9553adb6302
umarshakeb <umarshakeb@gmail.com>
2026-06-11 02:06:10 +0530

    updated requirements file and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatbot',
+    version='0.1.0',
+    author='Shakeb',
+    author_email="azmiumar@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
